0ffba2f11bc156bb27160be49eabbd80feb44641
hyeon <gusdn-2137@naver.com>
2025-02-04 09:48:08 +0900

    초기 커밋

---
+++ sl.js
@@ -0,0 +1,25 @@
+const content = "Hello, Eureca. . .";
+const text = document.querySelector(".text");
+let i = 0;
+
+function typing(){
+    let txt = content[i++];
+    text.innerHTML += txt=== "\n" ? "<br/>": txt;
+    if (i > content.length) {
+        text.textContent = "";
+        i = 0;
+    }
+}
+setInterval(typing, 200)
+
+
+
+
+// 네비게이션 바 클릭 시 사용할예정 
+$('a').click(function () {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+  return false;
+});
+
